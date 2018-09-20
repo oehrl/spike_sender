6cd88b7b9d6ba97f898c5252b225a8933d279ccc
Oehrl <oehrl@vr.rwth-aachen.de>
2018-09-20 11:33:12 +0200

    Added sending of the packets

--- spike_sender.cpp
+++ spike_sender.cpp
@@ -19,6 +19,9 @@ int main(int argc, char** argv) {
   // ms of simulation time per second
   const double speed = 10.0;
 
+  // Send x spikes at ones
+  const int spikes_per_package = 5;
+
   std::fstream file(argv[1]);
   if (!file) {
     std::cerr << "Failed to open file: " << argv[1] << std::endl;
@@ -43,6 +46,7 @@ int main(int argc, char** argv) {
   std::cout << "Start transmission!" << std::endl;
   using clock_t = std::chrono::steady_clock;
   const auto start_time = clock_t::now();
+  int recorded_spikes = 0;
   do {
     const auto current_time = clock_t::now();
     const std::chrono::duration<double> elapsed_time =
@@ -53,9 +57,21 @@ int main(int argc, char** argv) {
            current_iterator->time <= simulation_time) {
       std::cout << "Spike: " << current_iterator->neuron_id << std::endl;
       sd.Record(*current_iterator);
+      ++recorded_spikes;
+      if (recorded_spikes >= spikes_per_package) {
+        relay.Send(sd.node());
+        sd.node().reset();
+        recorded_spikes = 0;
+        std::cout << "Sending!" << std::endl;
+      }
       ++current_iterator;
     }
 
     std::cout << simulation_time << "/" << t_max << "ms" << std::endl;
   } while (simulation_time < t_max);
+
+  if (recorded_spikes > 0) {
+    relay.Send(sd.node());
+    std::cout << "Sending!" << std::endl;
+  }
 }
